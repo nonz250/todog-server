b253392b73477519872232b5168b5729bd04ead9
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-02-23 22:35:45 +0900

    topページのルーターとデザインを追加

---
+++ src/resources/js/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-text>
+                <p>Android端末の方は下のボタンからインストールできます。</p>
+                <p>iPhone端末の方はブラウザの<strong>「ホームに追加」</strong>を利用してください。</p>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer/>
+                <v-btn
+                    text
+                    outlined
+                    color="success"
+                >インストール
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+
+</style>
